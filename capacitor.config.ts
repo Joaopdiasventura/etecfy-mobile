ca96eb954a1553a2055684c82f469febe13a7995
João Paulo Dias <joaopdias.dev@gmail.com>
2025-10-16 02:24:56 -0300

    feat: add some assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'etecfy',
-  webDir: 'www'
+  appName: 'EtecFy',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      showSpinner: false,
+      backgroundColor: '#0b0b0c',
+      androidScaleType: 'CENTER_CROP',
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
 
 export default config;
